ff668f88059ea8fc745c69020ed52d452f77c1ed
Paula Rolim <rolim.paula1999@gmail.com>
2022-09-26 23:02:49 -0300

    feat: animated color transition

--- src/screens/Product/view.tsx
+++ src/screens/Product/view.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import {coffees} from './mocks';
 import {Page} from './components/Page';
 import Animated, {
+  interpolateColor,
   useAnimatedScrollHandler,
+  useAnimatedStyle,
   useSharedValue,
 } from 'react-native-reanimated';
 
@@ -11,18 +13,32 @@ const {width} = Dimensions.get('window');
 
 const circleSize = width + 50;
 const pageSize = width * 0.5;
+const colors = ['#736F26', '#01619E', '#F65403', '#FF8901', '#FFBD28'];
 
 export const ProductView = () => {
+  const currentIndex = useSharedValue(0);
   const translateX = useSharedValue(0);
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const backgroundColor = interpolateColor(
+      currentIndex.value,
+      colors.map((_, index) => index),
+      colors,
+    );
+    return {
+      backgroundColor,
+    };
+  });
 
   const scrollHandler = useAnimatedScrollHandler(event => {
     translateX.value = event.contentOffset.x;
+    currentIndex.value = event.contentOffset.x / pageSize;
   });
 
   return (
     <View style={styles.container}>
       <View style={styles.backgroundContainer}>
-        <View style={styles.circle} />
+        <Animated.View style={[styles.circle, animatedStyle]} />
       </View>
       <Animated.FlatList
         data={coffees}
@@ -57,7 +73,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
   },
   circle: {
-    backgroundColor: '#f00',
     width: circleSize,
     height: circleSize,
     borderRadius: circleSize / 2,
