41bca5b7e8d03ccb7fd5cddcb14fdbd6ca571fbb
Paula Rolim <rolim.paula1999@gmail.com>
2022-10-23 16:00:11 -0300

    feat: create auth context and provider

---
+++ src/contexts/Auth/provider.tsx
@@ -0,0 +1,26 @@
+import React, { useCallback, useMemo, useState } from 'react';
+import { AuthContext } from './context';
+import { AuthContextProps, AuthProviderProps, LoginParams, User } from './types';
+import { authLogin } from '../../services/authentication';
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [user, setUser] = useState<User | null>(null);
+
+  const login = useCallback(async ({ email, password }: LoginParams) => {
+    const response = await authLogin({ email, password });
+    if (response?.token) {
+      setUser({ name: 'Name from logged user' });
+    }
+  }, []);
+
+  const value: AuthContextProps = useMemo(
+    () => ({
+      isLoggedIn: Boolean(user),
+      user,
+      login,
+    }),
+    [user],
+  );
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+}
---
+++ src/contexts/Auth/types.ts
@@ -0,0 +1,21 @@
+/* eslint-disable no-unused-vars */
+import { ReactNode } from 'react';
+
+export interface LoginParams {
+  email: string;
+  password: string;
+}
+
+export interface User {
+  name: string;
+}
+
+export interface AuthContextProps {
+  isLoggedIn: boolean;
+  user: User | null;
+  login: ({ email, password }: LoginParams) => void;
+}
+
+export interface AuthProviderProps {
+  children: ReactNode;
+}
